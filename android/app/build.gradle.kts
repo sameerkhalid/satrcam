46ed42b712c15f185199defef2fe8afa7b14cb1d
sameerkhalid <sameer@zomentum.com>
2025-08-02 22:33:39 +0530

    ndk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,15 +8,15 @@ plugins {
 android {
     namespace = "com.example.satrcam"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.13846066"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
